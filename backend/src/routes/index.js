5e0fd2c2ae710dfbbd0b063e494953537ef516f9
Artur Wagner <arturwagner99@hotmail.com>
2026-01-07 13:45:31 +0000

    Backend Updated

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,8 +1,26 @@
 import { Router } from "express";
 import { registerUserController } from "../controllers/user.controller.js";
+import {
+  getUsersController,
+  loginUserController,
+  addUserResourcesController,
+  listUserResourcesController,
+  listUserResourcesByIdController,
+  updateResourceController,
+  addReservationController,
+} from "../controllers/user.controller.js";
 
 const router = Router();
 
 router.post("/register", registerUserController);
+router.post("/login", loginUserController);
+router.post("/resources", addUserResourcesController);
+router.post("/reservation", addReservationController);
+
+router.get("/users", getUsersController);
+router.get("/list", listUserResourcesController);
+router.get("/list/:id", listUserResourcesByIdController);
+
+router.put("/update/:id", updateResourceController);
 
 export default router;
